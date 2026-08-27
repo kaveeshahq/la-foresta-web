adbd5d4332c5a3abccc929c9fead7af0b8d20510
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2026-08-27 20:01:31 +0530

    chore: initialize La Foresta frontend

---
+++ components/providers/smooth-scroll-provider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
+
+type SmoothScrollProviderProps = {
+  children: ReactNode;
+};
+
+export function SmoothScrollProvider({
+  children,
+}: SmoothScrollProviderProps) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
+
+    let frameId: number;
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      frameId = requestAnimationFrame(raf);
+    };
+
+    frameId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(frameId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
